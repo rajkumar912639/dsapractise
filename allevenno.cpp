a9611d3b18ebba526cafb789284429bbfd88692b
Rajkumar rajbhar <114279134+rajkumar912639@users.noreply.github.com>
2024-02-20 19:55:44 +0530

    added

---
+++ allevenno.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the no to get all even no: ";
+    cin>>n;
+   for(int i=0;i<=n;i++)
+   {
+    int k=2;
+    cout<<k*i<<endl;
+   }
+    return 0;
+}
